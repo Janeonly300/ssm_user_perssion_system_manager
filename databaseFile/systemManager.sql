63801bebb06380a355e0f437c7a7daf37dac4fa6
jianyi <1261794911@qq.com>
2021-02-21 16:16:42 +0800

    更新数据库文件，新增会员、订单、旅客表

--- databaseFile/systemManager.sql
+++ databaseFile/systemManager.sql
@@ -16,3 +16,61 @@ CREATE TABLE t_product(
 	product_desc VARCHAR(250) , -- 描述
 	product_status INT  -- 状态 -0 关闭 1开启
 ) ENGINE=INNODB DEFAULT CHARSET utf8;
+
+SELECT * FROM t_product;
+
+/**
+	会员表
+**/
+DROP TABLE IF EXISTS t_member;
+CREATE TABLE t_member(
+	id VARCHAR(36) PRIMARY KEY , -- 主键uuid
+	member_name VARCHAR(20) , -- 姓名
+	member_nock_name VARCHAR(20) , -- 昵称
+	member_phone VARCHAR(20) , -- 电话
+	member_email VARCHAR(50)  -- 邮箱
+)ENGINE=INNODB DEFAULT CHARSET utf8;
+
+/**
+	旅客表
+**/
+DROP TABLE IF EXISTS t_traveller;
+CREATE TABLE t_traveller(
+	id VARCHAR(36) PRIMARY KEY , -- 主键id
+	traveller_name VARCHAR(20) , -- 姓名
+	traveller_sex VARCHAR(2) , -- 性别
+	traveller_phone VARCHAR(20) , -- 电话
+	traveller_credentials_type INT, -- 证件类型|0身份证|1护照|2军官证|
+	traveller_credentials_num VARCHAR(50) , -- 证件号码
+	traveller_type INT  -- 旅客类型|0成人|1儿童
+)ENGINE = INNODB DEFAULT CHARSET utf8;
+
+/**
+	订单表
+**/
+DROP TABLE IF EXISTS t_orders;
+CREATE TABLE t_orders(
+	id VARCHAR(36) PRIMARY KEY , -- 主键Id
+	orders_num VARCHAR(20) NOT NULL UNIQUE , -- 订单编号
+	orders_time DATETIME, -- 下单时间
+	orders_people_count INT  ,  -- 出行人数，
+	orders_desc INT , -- 订单描述
+	orders_pay_type INT , -- 支付方式 |0支付宝|1微信|2其他
+	ordes_status INT , -- 支付状态，|0 已经支付|2没有支付
+	product_id VARCHAR(36) , -- 产品id
+	member_id VARCHAR(36) ,-- 会员id
+	FOREIGN KEY (product_id) REFERENCES t_product(id),
+	FOREIGN KEY (member_id) REFERENCES t_member(id)
+) ENGINE=INNODB DEFAULT CHARSET utf8;
+
+/**
+	旅客和订单中间表
+**/
+DROP TABLE IF EXISTS t_orders_traveller();
+CREATE TABLE t_orders_traveller(
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	orders_id VARCHAR(36) , 
+	traveller_id VARCHAR(36),
+	FOREIGN KEY (traveller_id) REFERENCES t_traveller(id),
+	FOREIGN KEY (orders_id) REFERENCES t_orders(id)
+)ENGINE=INNODB DEFAULT CHARSET utf8;
